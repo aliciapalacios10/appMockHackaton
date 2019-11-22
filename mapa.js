3b63e8524e18a0d610e8390f504a39a5c01ae073
Daynizyvc <zinyad_leay@hotmail.com>
2019-11-22 13:32:13 -0600

    Se generan tajetas segun el filtrado

--- mapa.js
+++ mapa.js
@@ -9,6 +9,47 @@ let myPrice= document.querySelector(".precio")
 let myConnector=document.querySelector(".conector")
 let myState=document.querySelector(".estado")
 
+// Yael
+//FUncion para imprimir las tarjetas
+const printTarget = document.getElementById("target");
+
+let target = (data) => {
+    let str = '';
+    data.forEach(element =>{
+        let i=0;
+        str +=
+            `<div id="card${element.id}" class="container">
+                <div class="title">
+                    Mirna Guzman
+                </div>
+                <div class="date-marca" id="marca">
+                    ${element.name}
+                </div>
+                <div class="container_date">
+                    <div class="date" id="status">
+                        <img class="icon-target" src="img/electric-station.png">
+                        ${element.estatus.toUpperCase()}
+                    </div>
+                    <div class="date" id="type">
+                        <img class="icon-target" src="img/tools-and-utensils.png">
+                        ${element.cargador.slice(0,2)}
+                        </br>
+                        ${element.cargador.slice(3,8)}
+                    </div>
+                    <div class="date" id="price">
+                        <img class="icon-target" src="img/coin.png">
+                        ${element.costo}</div>
+                    </div>
+                    <div class="btn">
+                        <input type="button" class="button" id="btn_charge" value="CARGAR">
+                        <input type="button" class="button" id="btn_reserve" value="RESERVAR">
+                    </div>
+            </div>
+            `
+    });    
+    printTarget.innerHTML = str;
+}
+
 const getLocations = () => {
     fetch('https://cors-anywhere.herokuapp.com/api-electric-charger.herokuapp.com/electricCharger')
         .then(response => response.json())
@@ -29,25 +70,9 @@ const getLocations = () => {
 
                 locationsInfo.push(locationData);
 
-                console.log(location.name);
-
-                // locationsInfo.filter(location =>
-                //     location.cargador === carg
-                // )
+                // console.log(location.name);
 
             });
-
-            // console.log('COORDENADAS',locationInfo);
-
-
-            // let names = [];
-            // locations.forEach(location => {
-            //     let allNames = {
-            //             name:(location.name),
-            //     };
-            //     names.push(allNames)
-            // });
-            // console.log(names);
 
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition((data) => {
@@ -157,6 +182,8 @@ myPrice.addEventListener("click", (e) => {
     console.log(`Holis, el precio es ${event.target.innerText}`)
     filtered=locationsInfo.filter(location => location.costo === selectedPrice);
     console.log(filtered);
+    target(filtered)
+
 
         navigator.geolocation.getCurrentPosition((data)=>{
             let currentPosition = {
@@ -172,6 +199,7 @@ myConnector.addEventListener("click", (event) => {
     console.log(criterion);
     filtered=locationsInfo.filter(location => (location.cargador).includes(criterion));
     console.log(filtered);
+    target(filtered)
 
     // if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((data)=>{
@@ -188,6 +216,7 @@ myState.addEventListener("click", (event) => {
     console.log(`el estado es: ${estado}`);
     filtered=locationsInfo.filter(location => location.estatus === estado);
     console.log(filtered);
+    target(filtered)
 
     // if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((data)=>{
